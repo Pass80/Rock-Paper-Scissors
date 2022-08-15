d9f8d485e731d40791f3b123314c8e5647090696
Feras <ferassaleh1980@gmail.com>
2022-08-15 12:46:22 +0200

    remove some code lines

--- index.js
+++ index.js
@@ -115,65 +115,65 @@ scissorsElement.addEventListener('click', () => {
     }, 2000);
 });
 
-const playRound = (player1, computer) => {
-    player1 = player1.toLowerCase();
-    if (player1 === computer) {
-        console.log('No Winner - (Tie)');
-        return STATE_DRAW;
-    }
-    if (player1 === gameOptions[0] && computer === gameOptions[1]) {
-        console.log('You Win ! Rock beats Scissors. ');
-        return STATE_WIN;
-    }
-    if (player1 === gameOptions[0] && computer === gameOptions[2]) {
-        console.log('You Lose ! Paper beats Rock. ');
-        return STATE_LOSE;
-    }
-    if (player1 === gameOptions[2] && computer === gameOptions[1]) {
-        console.log('You Lose ! Scissors beats Paper. ');
-        return STATE_LOSE;
-    }
-    if (player1 === gameOptions[2] && computer === gameOptions[0]) {
-        console.log('You Win ! Paper beats Rock. ');
-        return STATE_WIN;
-    }
-    if (player1 === gameOptions[1] && computer === gameOptions[0]) {
-        console.log('You Lose ! Rock beats Scissors . ');
-        return STATE_LOSE;
-    }
-    if (player1 === gameOptions[1] && computer === gameOptions[2]) {
-        console.log('You Win ! Scissors beats Paper. ');
-        return STATE_WIN;
-    }
-};
+// const playRound = (player1, computer) => {
+//     player1 = player1.toLowerCase();
+//     if (player1 === computer) {
+//         console.log('No Winner - (Tie)');
+//         return STATE_DRAW;
+//     }
+//     if (player1 === gameOptions[0] && computer === gameOptions[1]) {
+//         console.log('You Win ! Rock beats Scissors. ');
+//         return STATE_WIN;
+//     }
+//     if (player1 === gameOptions[0] && computer === gameOptions[2]) {
+//         console.log('You Lose ! Paper beats Rock. ');
+//         return STATE_LOSE;
+//     }
+//     if (player1 === gameOptions[2] && computer === gameOptions[1]) {
+//         console.log('You Lose ! Scissors beats Paper. ');
+//         return STATE_LOSE;
+//     }
+//     if (player1 === gameOptions[2] && computer === gameOptions[0]) {
+//         console.log('You Win ! Paper beats Rock. ');
+//         return STATE_WIN;
+//     }
+//     if (player1 === gameOptions[1] && computer === gameOptions[0]) {
+//         console.log('You Lose ! Rock beats Scissors . ');
+//         return STATE_LOSE;
+//     }
+//     if (player1 === gameOptions[1] && computer === gameOptions[2]) {
+//         console.log('You Win ! Scissors beats Paper. ');
+//         return STATE_WIN;
+//     }
+// };
 
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+// const game = () => {
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        // const userSelection = window.prompt('Enter rock, scissors, or paper ');
-        const computerSelection = computerPlay();
-        const roundScore = playRound(userSelection, computerSelection);
-        if (roundScore === STATE_WIN) {
-            playerScore++;
-        } else if (roundScore === STATE_LOSE) {
-            computerScore++;
-        }
-    }
-    console.log('Your score : ', playerScore);
-    console.log('Computer score : ', computerScore);
+//     for (let i = 0; i < 5; i++) {
+//         // const userSelection = window.prompt('Enter rock, scissors, or paper ');
+//         const computerSelection = computerPlay();
+//         const roundScore = playRound(userSelection, computerSelection);
+//         if (roundScore === STATE_WIN) {
+//             playerScore++;
+//         } else if (roundScore === STATE_LOSE) {
+//             computerScore++;
+//         }
+//     }
+//     console.log('Your score : ', playerScore);
+//     console.log('Computer score : ', computerScore);
 
-    if (playerScore > computerScore) {
-        console.log('Final result : You win');
-        return STATE_WIN;
-    }
-    if (playerScore < computerScore) {
-        console.log('Final result : You lose');
-        return STATE_LOSE;
-    }
-    console.log('Final result : Draw');
-    return STATE_DRAW;
-};
+//     if (playerScore > computerScore) {
+//         console.log('Final result : You win');
+//         return STATE_WIN;
+//     }
+//     if (playerScore < computerScore) {
+//         console.log('Final result : You lose');
+//         return STATE_LOSE;
+//     }
+//     console.log('Final result : Draw');
+//     return STATE_DRAW;
+// };
 
 // game();
